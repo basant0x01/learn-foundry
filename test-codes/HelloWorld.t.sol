28c04390eebf9972bc714609447af08cba6492d1
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-11-19 13:03:20 +0545

    Add files via upload

---
+++ test-codes/HelloWorld.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/HelloWorld.sol";
+
+contract HelloWorldTest is Test {
+    HelloWorld public helloworld;
+
+    function setUp() public {
+        helloworld = new HelloWorld();
+    }
+
+    function test_Greet() public {
+        assertEq(helloworld.greet(), "Hello Basant!");
+    }
+
+}
